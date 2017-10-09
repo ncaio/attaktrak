f69425bd0dff1ddd1cdb0d3e518171b66a03a6b3
Noilson Caio <caiogore@gmail.com>
2017-10-09 16:07:22 -0300

    multiple wifi cards detection

--- scripts/interface.sh
+++ scripts/interface.sh
@@ -2,29 +2,35 @@
 #
 #
 #
-_interface="$(iw dev| head -n 1|tr -d '\#')"
+_interface="$(iw dev | grep ^phy | tr -d '\#' | paste -s -d " ")"
 #
 #
 #
-echo "INTERFACE: $_interface"
 if [ ! -z "$_interface" ]
 then
-	_mode="$(iw phy "$_interface" info | grep -A12 "Supported interface modes:" | grep -B 12 "Band 1:")"
-	echo "$_mode" | grep -q "* monitor"
-	_key="$?"
-	if [ "$_key" != "0" ]
-	then
-		echo "Monitor mode disabled"
-		exit 1
-	else
-		_mssid=$(iw phy "$_interface" info | grep 'total.*channels' | cut -d',' -f1 | awk '{print $NF}')
+	for i in $_interface
+	do
+		echo "--------------------------------------"
+		echo "INTERFACE: $i"
+		_mode="$(iw phy "$i" info | grep -A12 "Supported interface modes:" | grep -B 12 "Band 1:")"
+		echo "$_mode" | grep -q "* monitor"
+		_key="$?"
+		if [ "$_key" != "0" ]
+		then
+			echo "Monitor mode - not supported"
+			exit 1
+		else
+			_mssid=$(iw phy "$i" info | grep 'total.*channels' | cut -d',' -f1 | awk '{print $NF}')
 		if [ -z "$_mssid" ]
 		then
 			_nssid="1"
 		else
 			_nssid="$_mssid"
 		fi
-		echo "SSID SIZE: $_nssid SSIDs would be supported"
+		echo "SSID SIZE: $_nssid SSIDs may be supported"
 	fi
+	done
+	echo "--------------------------------------"
+	echo ""
 fi
 
