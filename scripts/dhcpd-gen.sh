03901bb22c0d8d640818e83f7a8da281885bf1c0
Noilson Caio <caiogore@gmail.com>
2017-10-10 14:32:00 -0300

    dhcpd.con script generator

---
+++ scripts/dhcpd-gen.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#
+#
+#
+_nssid="$1"
+_count="1"
+#
+#
+#
+echo "
+#
+#	DHCPD.CONF HEADER
+#
+ddns-update-style none;
+ignore client-updates;
+authoritative;
+default-lease-time 600;
+max-lease-time 3600;
+#
+#	/30 TO MANAGEMENT SSID
+#
+subnet 10.10.0.0 netmask 255.255.255.252 {
+	range 10.10.0.2 10.10.0.2;
+}
+"
+#
+#
+#
+_bit="1"
+#
+#
+#
+for _i in $(cat ../templates/SSID|head -n "$_nssid")
+do 
+	echo "
+#
+#
+#
+subnet 10.10."$_bit".0 netmask 255.255.255.0 {
+on commit {
+        set ClientIP = binary-to-ascii(10, 8, ".", leased-address);
+	set ClientMac = binary-to-ascii(16, 8, ":", substring(hardware, 1, 6));
+	log(concat("Commit: IP: ", ClientIP, " Mac: ", ClientMac));
+	execute("/usr/local/scripts/script.sh", ClientIP, ClientMac, "$_i", option host-name);
+	}
+	range 10.10."$_bit".10 10.10."$_bit".254;
+	option routers 10.10."$_bit".1;
+	}
+"
+	let _bit++
+done
