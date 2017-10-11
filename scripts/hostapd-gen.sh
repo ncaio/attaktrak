867603f90c1e02d106180094d91f95a08bf79ad5
Noilson Caio <caiogore@gmail.com>
2017-10-11 15:09:18 -0300

    hostapd GEN

---
+++ scripts/hostapd-gen.sh
@@ -0,0 +1,34 @@
+#!/bin/bash 
+#
+#
+_interw="$1"
+_macm="$2"
+_nssid="$3"
+#
+#
+#
+echo "interface="$_interw"
+bssid="$_macm"
+driver=nl80211
+ssid=RaspiGET
+channel=3
+wpa=2
+wpa_passphrase=raspgetpi
+wpa_key_mgmt=WPA-PSK
+rsn_pairwise=CCMP"
+#
+#
+#
+_bit="1"
+#
+#
+#
+for _i in $(cat ../templates/SSID|head -n "$_nssid")
+do
+	echo "#
+#	SSID $_i
+#
+bss="$_interw"_"$_bit"
+ssid="$_i""
+	let _bit++
+done
