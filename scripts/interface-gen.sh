e5f0862cdb87842acf1c1ab62008b70bee589d5f
Noilson Caio <caiogore@gmail.com>
2017-10-13 11:48:36 -0300

    gerador de arquivo de configuracao interfaces

---
+++ scripts/interface-gen.sh
@@ -0,0 +1,37 @@
+#!/bin/bash 
+#
+#
+_interw="$1"
+_macm="$2"
+_nssid="$3"
+#
+#
+#
+echo "#
+#	RaspiGET CONFIG
+#
+auto $_interw
+allow-hotplug $_interw
+iface wlan0 inet static
+hostapd /etc/hostapd/hostapd.conf
+address 10.10.0.1
+netmask 255.255.255.0
+pre-up ifconfig $_interw hw ether $_macm"
+#
+#
+#
+_bit="1"
+#
+#
+#
+for _i in $(seq 1 $(($_nssid-1)) )
+do
+	echo "#
+#	$_interw alias $_i	
+#
+auto $_interw"_"$_i
+iface $_interw"_"$_i inet static
+address 10.10.$_i.1
+netmask 255.255.255.0"
+	let _bit++
+done
